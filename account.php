cd9baa3c39f05d23a02d9b5aa5e72131f3cfb56a
jumakrk <jumakrk@gmail.com>
2024-05-20 15:32:51 +0300

    added classes code

--- account.php
+++ account.php
@@ -1,4 +1,18 @@
 <!-- Classes in PHP -->
 <?php
-class Account {}
+class Account {
+    // A class has properties (Variables)
+    // Access mudifiers (public, private, protected)
+    public $account_number = 4256473649901235;
+    public $account_name = "John Doe";
+
+    // A class has methods
+    public function get_account_number(){
+        // A method can access the properties of the class ("->"object access operator)
+        echo $this->account_number;
+    }
+}
+
+    // create an object
+    $account = new Account();
 ?>
